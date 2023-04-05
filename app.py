7993bc68b8b5f6abf22a034fdccf048970efa8e2
Uvidu Adurajith <uvidu.20211188@iit.ac.lk>
2023-04-05 09:56:02 +0530

    edited the app.py full file

--- app.py
+++ app.py
@@ -3,45 +3,38 @@ from flask import request
 from flask import Flask, render_template, url_for
 from flask import request as req            #req is used for communication between the front end and the back end
 import requests
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
-from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__,static_url_path="/static/css/GUI3.css")
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reviews.db'
-#db = SQLAlchemy(app)
 
 model = joblib.load('sentiment-analysis.joblib')
 vectorizer = joblib.load('vectorizer.joblib')
 
-#class Review(db.Model):
-    #id = db.Column(db.Integer, primary_key=True)
-    #original_review = db.Column(db.String(1000))
-    #summarized_review = db.Column(db.String(1000))
-
-#backend that connects Emotion detection page 
 @app.route('/Emotion')
 def home():
     return render_template('Emotion.html')
 
-#decorator that directs the user to view the sentiment after clicking the button
 @app.route('/predict', methods=['POST'])
 def predict():
     text = request.form['text']
     text = vectorizer.transform([text])
     prediction = model.predict(text)[0]
-    
-# Save the original and summarized review to the database
-    #review = Review(original_review=text, summarized_review=prediction)
-    #db.session.add(review)
-    #db.session.commit()
-    #return render_template('Emotion.html', prediction=prediction)
+    return render_template('Emotion.html', prediction=prediction)
 
-#@app.route('/reviews')
-#def reviews():
-   # reviews = Review.query.all()
-   # return render_template('review.html', reviews=reviews)
+@app.route("/",methods = ["GET","POST"])
+def Index():
+    return(render_template("EmoRabbit.html"))
+
+@app.route("/AboutUs")
+def AboutUs():
+    return(render_template("AboutUs.html"))
+
+@app.route("/database")
+def Database():
+    return(render_template("News.html"))
 
 @app.route("/SignUp")
 def SignUp():
@@ -55,39 +48,39 @@ def SignUp():
            flash('Record was successfully added')   
     return(render_template("signup.html"))
 
-@app.route("/AboutUs")
-def AboutUs():
-    return(render_template("AboutUs.html"))
+
 
 @app.route("/get_summary",methods=["GET","POST"]) 
 def get_summary():
     if req.method=="POST":
-        input_text = request.form["input_text"]
         # The token to the pretrained model
         API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
         headers = {"Authorization": f"Bearer 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"}
 
-#send the query to the model and get the response
+        #send the query to the ML model and getting the response
         def query(payload):
             response = requests.post(API_URL, headers=headers, json=payload)
             return response.json()
-#load the query using the input text, minimum and maximum lengths of the summary
+            
+        #input data has to be taken from 'form' in the front end
+        #use the variable name 'input_text' given in the front end
         input_text = req.form["input_text"] 
         max_len = 1000
         min_len = 20
         
-#load the query using the input text, minimum and maximum lengths of the summary
+        #load the query using the input text, minimum and maximum lengths of the summary
         output = query({
             "inputs": input_text,
             "parameters": {"min_length" : min_len, "max_length" : max_len},
         })[0]
         #return the summarized output 
         #use the variable names 'result' as given in the front end
-        return render_template("GUI3.html", result = output["summary_text"]) 
+        return render_template("EmoRabbit.html", result = output["summary_text"]) 
     else:
-        return(render_template("GUI3.html")) 
-     
+        return(render_template("EmoRabbit.html"))
+
+
 if __name__ == '__main__':
    # db.create_all()
     app.debug = True #should be true for testing, should be false for production
-    app.run()          
+    app.run()
